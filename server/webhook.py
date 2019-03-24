69d85cb108e3b708983f0f22f14df20e03afed7a
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2019-03-23 20:28:31 -0700

    Update webhook endpoint for finding song - Add and tweak tests for webhook endpoints

--- server/webhook.py
+++ server/webhook.py
@@ -23,20 +23,27 @@ app = Flask(__name__)
 assist = Assistant(app, route='/webhook')
 music_api = KnowledgeBaseAPI('knowledge_base/knowledge_base.db')
 
-@assist.action('About Song', mapping=dict(song_name='song'))
-def get_song_info(song_name):
-    """Fetches song info for specified song.
+@assist.action('Find Song')
+def find_song(song, artist):
+    """Fetches Spotify URI for specified song.
 
     Params:
-        song_name (str): e.g. "thank u, next"
+        song (str): e.g. "thank u, next"
     """
-    hits = music_api.get_song_data(song_name)
+    hits = music_api.get_song_data(song)
     if len(hits) == 0:
-        return tell(f"Unfortunately, I couldn't find any info about {song_name}.")
+        return tell(f"Unfortunately, I couldn't find {song}.")
     else:
-        artist = hits[0]['artist_name']
-        spotify_uri = hits[0]['spotify_uri']
-        return tell(f"Playing {song_name} by {artist}?").link_out("Spotify", spotify_uri)
+        if artist is not None:
+            for hit in hits:
+                if hit['artist_name'] == artist:
+                    return tell(f"Found {song} by {artist}").link_out("Spotify", hit['spotify_uri'])
+            return tell(f"Unfortunately, I couldn't find {song} by {artist}.")
+
+        else:
+            cur_artist = hits[0]['artist_name']
+            spotify_uri = hits[0]['spotify_uri']
+            return tell(f"Found {song} by {cur_artist}").link_out("Spotify", spotify_uri)
 
 @assist.action('Get More Obscure Song', mapping=dict(song_name='song'))
 def get_more_obscure_song(song_name):
@@ -56,9 +63,6 @@ def get_more_obscure_song(song_name):
     more_obscure_songs = music_api.get_less_popular_songs(song_name)
     similar_artists = music_api.get_related_entities(artist_name)
 
-    print(f"Songs more obscure than {song_name}: {[x['song_name'] for x in more_obscure_songs]}")
-    print(f"Similar artists to {artist_name}: {similar_artists}")
-
     results = []
     for tmp_song_data in more_obscure_songs:
         if tmp_song_data['artist_name'] in similar_artists:
@@ -70,7 +74,7 @@ def get_more_obscure_song(song_name):
         res_song_name = results[0]['song_name']
         res_artist_name = results[0]['artist_name']
         spotify_uri = results[0]['spotify_uri']
-        return tell(f"Playing {res_song_name} by {res_artist_name}?").link_out("Spotify", spotify_uri)
+        return tell(f"Found {res_song_name} by {res_artist_name}").link_out("Spotify", spotify_uri)
 
 
 if __name__ == '__main__':
--- tests/server/test_webhook.py
+++ tests/server/test_webhook.py
@@ -10,11 +10,7 @@ class TestWebhook(unittest.TestCase):
         with self.test_app.app_context():
             res = endpoint.get_more_obscure_song("thank u, next")
         msg = res._messages[0]['text']['text'][0]
-        self.assertNotEqual(
-            msg,
-            "Unfortunately, I couldn't find any songs similar to but more obscure than thank u, next.",
-            "Woops!",
-        )
+        self.assertIn("Found", msg, "Expected to find a song.")
 
     def test_get_more_obscure_song_none_found(self):
         with self.test_app.app_context():
@@ -23,5 +19,45 @@ class TestWebhook(unittest.TestCase):
         self.assertEqual(
             msg,
             "Unfortunately, I couldn't find any songs similar to but more obscure than If Only.",
-            "Woops!",
+            "Expected not to find a song.",
+        )
+
+    def test_find_song(self):
+        with self.test_app.app_context():
+            res = endpoint.find_song("thank u, next", artist=None)
+        msg = res._messages[0]['text']['text'][0]
+        self.assertEqual(
+            msg,
+            "Found thank u, next by Ariana Grande",
+            "Expected to find a song.",
+        )
+
+    def test_find_song_with_artist(self):
+        with self.test_app.app_context():
+            res = endpoint.find_song("thank u, next", artist="Ariana Grande")
+        msg = res._messages[0]['text']['text'][0]
+        self.assertEqual(
+            msg,
+            "Found thank u, next by Ariana Grande",
+            "Expected to find a song.",
+        )
+
+    def test_find_song_with_wrong_artist(self):
+        with self.test_app.app_context():
+            res = endpoint.find_song("thank u, next", artist="Alessia Cara")
+        msg = res._messages[0]['text']['text'][0]
+        self.assertEqual(
+            msg,
+            "Unfortunately, I couldn't find thank u, next by Alessia Cara.",
+            "Expected to find a song.",
+        )
+
+    def test_find_song_unknown(self):
+        with self.test_app.app_context():
+            res = endpoint.find_song("some unknown song", artist=None)
+        msg = res._messages[0]['text']['text'][0]
+        self.assertEqual(
+            msg,
+            "Unfortunately, I couldn't find some unknown song.",
+            "Expected to find a song.",
         )
