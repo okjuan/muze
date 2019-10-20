6cb421ee3b67bb899f61cba61957bb37f0362ea6
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2019-10-20 01:14:09 -0700

    Encapsulate DOM manipulation logic in new View module

--- app/static/main.mjs
+++ app/static/main.mjs
@@ -1,24 +1,18 @@
 // Author: Juan Carlos Gallegos.
 
 import { GetPlayer } from './player.mjs'
+import { View } from './view.mjs'
+
+const State = {
+  Playing: false
+}
 
 const Player = GetPlayer(({songName, artistName, albumName, albumArtLink, songLink}) => {
-  let elem = $("#track-name");
-  elem.text(`${songName}`);
-
-  elem = $("#artist-name");
-  elem.text(`${artistName}`);
-
-  elem = $("#album-name");
-  elem.text(`${albumName}`);
-
-  let albumArtElem = $('#album-art');
-  albumArtElem.attr('src', albumArtLink);
-
-  let songLinkElem = $('#album-art-link');
-  songLinkElem.attr('href', songLink);
-
-  $('#music-metadata-container').css('display', 'block');
+  View.UpdateCurrentlyPlaying({
+    song: {name: songName, link: songLink},
+    artist: {name: artistName},
+    album: {name: albumName, coverLink: albumArtLink}
+  })
 });
 
 window.onSpotifyWebPlaybackSDKReady = Player.Init;
@@ -29,76 +23,36 @@ socket.on('connect', () => {
 });
 
 socket.on('play song', (data) => {
+  if (State.Playing == false) {
+    // TODO: confirm that indeed there is a song playing BEFORE presenting options
+    View.PresentRecommendationControls(recommendationHandler, () => socket.emit("get random song"));
+  }
   Player.PlaySong(data['spotify_uri']);
-  $('.loading').removeClass('loading');
-  // TODO: confirm that indeed there is a song playing BEFORE presenting options
-  presentRecommendationOptions();
+  State.Playing = true;
+  View.SetState({loading: false});
 });
 
-// TODO: present messages in a user-friendly manner
+const recommendationHandler = (recommendType) => {
+  Player.GetCurrentSong().then(({spotify_uri, name}) => {
+    // TODO: handle case where Promise does not resolve nicely
+    socket.emit('get recommendation', {
+      'song': name,
+      'spotify_uri': spotify_uri,
+      'adjective': recommendType
+    });
+  });
+}
+
 socket.on('msg', (msgStr) => {
-  $('.loading').removeClass('loading');
+  View.SetState({loading: false});
+  // TODO: present messages in a user-friendly manner
   alert(msgStr);
 })
 
-$(document).ready(() => {
-  $('#random-song').on('click', () => {
+View.OnReady(() => {
+  View.PresentSinglePlayButton(() => {
     Player.Connect(() => {
       socket.emit('get random song');
-      $('#random-song').addClass('loading');
-    });
+    })
   });
-});
-
-const presentRecommendationOptions = () => {
-  let randSongBtn = $('#random-song');
-  if (randSongBtn.length === 0) {
-    return;
-  }
-  randSongBtn.hide('slow', () => {
-    randSongBtn.after(getRecommendationButtons());
-    randSongBtn.remove();
-  });
-}
-
-const setElemClass = (id, cssClass) => {
-  $('#'+id).addClass(cssClass);
-}
-
-const getRecommendationButtons = () => {
-  let recommendationButtons = [];
-  for (let btn of [
-    {adj: undefined,        title: 'More like this',         id: 'similar-rec-btn'},
-    {adj: 'less acoustic',  title: 'More electric!',         id: 'more-electric-rec-btn'},
-    {adj: 'more acoustic',  title: 'No, more acoustic',      id: 'more-acoustic-rec-btn'},
-    {adj: 'less popular',   title: 'Actually, more obscure', id: 'more-obscure-rec-btn'},
-    {adj: 'more popular',   title: 'More mainstream please', id: 'more-popular-rec-btn'},
-    {adj: 'happier',        title: 'Damn, cheer up',         id: 'happier-rec-btn'},
-    {adj: 'sadder',         title: 'Make it sad :(',         id: 'sadder-rec-btn'},
-    {adj: 'dancier',        title: 'I wanna dance to it!',   id: 'dancier-rec-btn'},
-  ]) {
-    recommendationButtons.push($('<button></button>')
-      .attr('id', btn.id)
-      .addClass("btn btn-primary btn-sm m-2")
-      .text(btn.title)
-      .on('click', () => {
-        setElemClass(btn.id, 'loading');
-        Player.GetCurrentSong().then(({spotify_uri, name}) => {
-          // TODO: handle case where Promise does not resolve nicely
-          socket.emit('get recommendation', {
-            'song': name,
-            'spotify_uri': spotify_uri,
-            'adjective': btn.adj
-          });
-        });
-      })
-    );
-  }
-  recommendationButtons.push($('<button></button>')
-    .attr('id', 'random-rec-btn')
-    .addClass("btn btn-sm m-2")
-    .text("Just random!")
-    .on('click', () => { socket.emit('get random song'); })
-  );
-  return recommendationButtons;
-};
+})
---
+++ app/static/view.mjs
@@ -0,0 +1,89 @@
+const View = {
+  RecommendationButtons: undefined,
+  OnReady: (loadHandler) => {
+    $(document).ready(loadHandler);
+  },
+  UpdateCurrentlyPlaying: ({ song, artist, album }) => {
+    let elem = $("#track-name");
+    elem.text(`${song.name}`);
+
+    elem = $("#artist-name");
+    elem.text(`${artist.name}`);
+
+    elem = $("#album-name");
+    elem.text(`${album.name}`);
+
+    let albumArtElem = $('#album-art');
+    albumArtElem.attr('src', album.coverLink);
+
+    let songLinkElem = $('#album-art-link');
+    songLinkElem.attr('href', song.link);
+
+    $('#music-metadata-container').css('display', 'block');
+  },
+  PresentSinglePlayButton: (clickHandler) => {
+    // view is loaded as default view in HTML
+    $('#random-song').on('click', () => {
+      setElemClass('random-song', 'loading');
+      clickHandler();
+    });
+  },
+  PresentRecommendationControls: (recommendationHandler, randomSongHandler) => {
+    if (View.RecommendationButtons === undefined) {
+      View.RecommendationButtons = getRecommendationButtons(recommendationHandler, randomSongHandler);
+    }
+    let randSongBtn = $('#random-song');
+    if (randSongBtn.length === 0) {
+      return;
+    }
+    randSongBtn.hide('slow', () => {
+      randSongBtn.after(View.RecommendationButtons);
+      randSongBtn.remove();
+    });
+  },
+  SetState: ({loading}) => {
+    if (loading === false) {
+      $('.loading').removeClass('loading');
+    }
+  }
+}
+
+const setElemClass = (id, cssClass) => {
+  $('#'+id).addClass(cssClass);
+}
+
+const getRecommendationButtons = (recommendationHandler, randomSongHandler) => {
+  let recommendationButtons = [];
+  for (let btn of [
+    {adj: undefined,        title: 'More like this',         id: 'similar-rec-btn'},
+    {adj: 'less acoustic',  title: 'More electric!',         id: 'more-electric-rec-btn'},
+    {adj: 'more acoustic',  title: 'No, more acoustic',      id: 'more-acoustic-rec-btn'},
+    {adj: 'less popular',   title: 'Actually, more obscure', id: 'more-obscure-rec-btn'},
+    {adj: 'more popular',   title: 'More mainstream please', id: 'more-popular-rec-btn'},
+    {adj: 'happier',        title: 'Damn, cheer up',         id: 'happier-rec-btn'},
+    {adj: 'sadder',         title: 'Make it sad :(',         id: 'sadder-rec-btn'},
+    {adj: 'dancier',        title: 'I wanna dance to it!',   id: 'dancier-rec-btn'},
+  ]) {
+    recommendationButtons.push($('<button></button>')
+      .attr('id', btn.id)
+      .addClass("btn btn-primary btn-sm m-2")
+      .text(btn.title)
+      .on('click', () => {
+        setElemClass(btn.id, 'loading');
+        recommendationHandler(btn.adj);
+      })
+    );
+  }
+  recommendationButtons.push($('<button></button>')
+    .attr('id', 'random-rec-btn')
+    .addClass("btn btn-sm m-2")
+    .text("Just random!")
+    .on('click', () => {
+      setElemClass('random-rec-btn', 'loading');
+      randomSongHandler();
+    })
+  );
+  return recommendationButtons;
+};
+
+export { View }
