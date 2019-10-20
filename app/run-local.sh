5058a7636054eae9bb279f0238a8bf92b8af5723
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2019-10-19 18:16:49 -0700

    Reorganize directory structure for client JS

--- app/run-local.sh
+++ app/run-local.sh
@@ -10,10 +10,10 @@ SSH_PID=$!
 
 # HACK: point to dev environment URLs
 echo "Configuring for local testing..."
-cat app/static/player.js | sed s/"https:\/\/muze-player\.herokuapp\.com"/"https:\/\/muze.serveo.net"/ > temp_player.js
-mv temp_player.js app/static/player.js
-cat app/static/index.js | sed s/"https:\/\/muze-player\.herokuapp\.com"/"http:\/\/localhost:5000"/ > temp_index.js
-mv temp_index.js app/static/index.js
+cat app/static/modules/player.mjs | sed s/"https:\/\/muze-player\.herokuapp\.com"/"https:\/\/muze.serveo.net"/ > temp_player.js
+mv temp_player.js app/static/modules/player.mjs
+cat app/static/modules/index.mjs | sed s/"https:\/\/muze-player\.herokuapp\.com"/"http:\/\/localhost:5000"/ > temp_index.js
+mv temp_index.js app/static/modules/index.mjs
 
 # lil trick to open browser after server is started (next cmd)
 sleep 1 && open "http://localhost:5000" &
@@ -23,7 +23,7 @@ python app/server.py
 echo "Closing ssh tunnel.."
 kill $SSH_PID
 
-cat app/static/player.js | sed s/"https:\/\/muze.serveo.net"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_player.js
-mv temp_player.js app/static/player.js
-cat app/static/index.js | sed s/"http:\/\/localhost:5000"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_index.js
-mv temp_index.js app/static/index.js
+cat app/static/modules/player.mjs | sed s/"https:\/\/muze.serveo.net"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_player.js
+mv temp_player.js app/static/modules/player.mjs
+cat app/static/modules/index.mjs | sed s/"http:\/\/localhost:5000"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_index.js
+mv temp_index.js app/static/modules/index.mjs
