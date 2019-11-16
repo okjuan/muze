4d8a307ae5f76d678039e401f14f2252bf136386
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2019-11-16 02:46:07 -0800

    Tweak test URL name

--- app/run-local.sh
+++ app/run-local.sh
@@ -5,12 +5,12 @@ export DIALOGFLOW_PROJECT_ID=$MUZE_PROJECT_ID
 
 # run in background
 echo "Opening ssh tunnel for port forwarding at muze.serveo.net"
-ssh -N -R muze:80:localhost:5000 serveo.net >/dev/null 2>&1 &
+ssh -N -R muze-player:80:localhost:5000 serveo.net >/dev/null 2>&1 &
 SSH_PID=$!
 
 # HACK: point to dev environment URLs
 echo "Configuring for local testing..."
-cat app/static/player.mjs | sed s/"https:\/\/muze-player\.herokuapp\.com"/"https:\/\/muze.serveo.net"/ > temp_player.js
+cat app/static/player.mjs | sed s/"https:\/\/muze-player\.herokuapp\.com"/"https:\/\/muze-player.serveo.net"/ > temp_player.js
 mv temp_player.js app/static/player.mjs
 cat app/static/main.mjs | sed s/"https:\/\/muze-player\.herokuapp\.com"/"http:\/\/localhost:5000"/ > temp_main.js
 mv temp_main.js app/static/main.mjs
@@ -23,7 +23,7 @@ python app/server.py
 echo "Closing ssh tunnel.."
 kill $SSH_PID
 
-cat app/static/player.mjs | sed s/"https:\/\/muze.serveo.net"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_player.js
+cat app/static/player.mjs | sed s/"https:\/\/muze-player.serveo.net"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_player.js
 mv temp_player.js app/static/player.mjs
 cat app/static/main.mjs | sed s/"http:\/\/localhost:5000"/"https:\/\/muze-player\.herokuapp\.com"/ > temp_main.js
 mv temp_main.js app/static/main.mjs
