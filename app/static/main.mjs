6a1d3381a2d16ef600805321da130e6425ac4b03
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-04-04 00:44:21 -0700

    Use 4 spaces as tab everywhere in JS files

--- app/static/main.mjs
+++ app/static/main.mjs
@@ -6,107 +6,107 @@ import { View } from './view.mjs'
 
 let bearerToken = SpotifyAuthHelper.GetBearerTokenFromUrl();
 if (bearerToken === undefined) {
-  SpotifyAuthHelper.RedirectToLogin({
-    authEndpointTemplate: SpotifyConfig.authEndpointTemplate,
-    clientId: SpotifyConfig.Auth.ClientId,
-    authScopes: SpotifyConfig.Auth.Scopes,
-    redirectUrl: AppConfig.AppIndexUrl
-  });
+    SpotifyAuthHelper.RedirectToLogin({
+        authEndpointTemplate: SpotifyConfig.authEndpointTemplate,
+        clientId: SpotifyConfig.Auth.ClientId,
+        authScopes: SpotifyConfig.Auth.Scopes,
+        redirectUrl: AppConfig.AppIndexUrl
+    });
 }
 Player.BearerToken = bearerToken;
 
 window.onSpotifyWebPlaybackSDKReady = Player.Init;
 
 View.OnReady(() => {
-  View.PresentSinglePlayButton({clickHandler: () => socket.emit('get random song')});
+    View.PresentSinglePlayButton({ clickHandler: () => socket.emit('get random song') });
 })
 
 const PlaylistEditor = GetPlaylistEditor({
-  urlTemplateForAddingTracksToPlaylist: SpotifyConfig.EndpointTemplates.AddTracksToPlaylist,
-  bearerToken: bearerToken
+    urlTemplateForAddingTracksToPlaylist: SpotifyConfig.EndpointTemplates.AddTracksToPlaylist,
+    bearerToken: bearerToken
 });
 
 const State = {
-  Streaming: false
+    Streaming: false
 };
 
 var socket = io.connect(AppConfig.AppIndexUrl);
 socket.on('connect', () => {
-  socket.emit('start session');
+    socket.emit('start session');
 });
 
 socket.on('play song', async (data) => {
-  try {
-    await Player.PlaySong({spotify_uri: data['spotify_uri']});
-  } catch (err) {
-    // TODO: send err as telemetry
-    return;
-  }
-  if (State.Streaming == false) {
-    // TODO: confirm that indeed there is a song playing BEFORE presenting options
-    // - surely I should be checking something like Player.IsPlaying?
-    View.PresentRecommendationControls({
-      recommendationHandler: recommendationHandler,
-      randomSongHandler: () => socket.emit("get random song"),
-    });
-    View.PresentPlaylistEditorControls({addSongHandler: addSongHandler});
-  }
-  State.Streaming = true;
+    try {
+        await Player.PlaySong({ spotify_uri: data['spotify_uri'] });
+    } catch (err) {
+        // TODO: send err as telemetry
+        return;
+    }
+    if (State.Streaming == false) {
+        // TODO: confirm that indeed there is a song playing BEFORE presenting options
+        // - surely I should be checking something like Player.IsPlaying?
+        View.PresentRecommendationControls({
+            recommendationHandler: recommendationHandler,
+            randomSongHandler: () => socket.emit("get random song"),
+        });
+        View.PresentPlaylistEditorControls({ addSongHandler: addSongHandler });
+    }
+    State.Streaming = true;
 
-  // code smell: is it really necessary to expose this method? couldn't we instead
-  //             update the state when the View updates?
-  View.SetState({loading: false}); // code smell!!!
+    // code smell: is it really necessary to expose this method? couldn't we instead
+    //             update the state when the View updates?
+    View.SetState({ loading: false }); // code smell!!!
 });
 
 socket.on('msg', (msgStr) => {
-  View.SetState({loading: false}); // code smell!!!
-  View.PresentMessage(msgStr);
+    View.SetState({ loading: false }); // code smell!!!
+    View.PresentMessage(msgStr);
 });
 
-Player.OnSongChange = ({songName, artistName, albumName, albumArtLink, songLink}) => {
-  View.UpdateCurrentlyPlaying({
-    song: {name: songName, link: songLink},
-    artist: {name: artistName},
-    album: {name: albumName, coverLink: albumArtLink}
-  })
+Player.OnSongChange = ({ songName, artistName, albumName, albumArtLink, songLink }) => {
+    View.UpdateCurrentlyPlaying({
+        song: { name: songName, link: songLink },
+        artist: { name: artistName },
+        album: { name: albumName, coverLink: albumArtLink }
+    })
 };
 
-const recommendationHandler = ({recommendType}) => {
-  try {
-    Player.GetCurrentSong().then(({spotify_uri, name}) => {
-      // TODO: handle case where Promise does not resolve nicely
-      socket.emit('get recommendation', {
-        'song': name,
-        'spotify_uri': spotify_uri,
-        'adjective': recommendType
-      });
-    });
-  } catch (err) {
-    // TODO: send err as telemetry
-    View.PresentMessage("I... can't think of anything right now. Ask me again later :~)");
-  }
+const recommendationHandler = ({ recommendType }) => {
+    try {
+        Player.GetCurrentSong().then(({ spotify_uri, name }) => {
+            // TODO: handle case where Promise does not resolve nicely
+            socket.emit('get recommendation', {
+                'song': name,
+                'spotify_uri': spotify_uri,
+                'adjective': recommendType
+            });
+        });
+    } catch (err) {
+        // TODO: send err as telemetry
+        View.PresentMessage("I... can't think of anything right now. Ask me again later :~)");
+    }
 }
 
 const addSongHandler = () => {
-  try {
-    Player.GetCurrentSong().then(({spotify_uri, name}) => {
-      // TODO: handle case where Promise does not resolve nicely
-      try {
-        PlaylistEditor.AddSong({
-          // TODO: parametrize. (placeholder: 'fizz buzz tangle' by jcgalleg)
-          spotifyPlaylistId: "2ZjWb4BpsCMNX22waSfNuq",
-          spotifyTrackUri: spotify_uri,
+    try {
+        Player.GetCurrentSong().then(({ spotify_uri, name }) => {
+            // TODO: handle case where Promise does not resolve nicely
+            try {
+                PlaylistEditor.AddSong({
+                    // TODO: parametrize. (placeholder: 'fizz buzz tangle' by jcgalleg)
+                    spotifyPlaylistId: "2ZjWb4BpsCMNX22waSfNuq",
+                    spotifyTrackUri: spotify_uri,
+                });
+                View.PresentMessage(`Added '${name}' to your playlist!`)
+            } catch (err) {
+                // TODO: send err as telemetry
+                View.PresentMessage(`Sorry, couldn't add '${name}' to your playlist.`)
+            } finally {
+                View.SetState({ loading: false }); // code smell!!!
+            }
         });
-        View.PresentMessage(`Added '${name}' to your playlist!`)
-      } catch (err) {
+    } catch (err) {
         // TODO: send err as telemetry
         View.PresentMessage(`Sorry, couldn't add '${name}' to your playlist.`)
-      } finally {
-        View.SetState({loading: false}); // code smell!!!
-      }
-    });
-  } catch (err) {
-    // TODO: send err as telemetry
-    View.PresentMessage(`Sorry, couldn't add '${name}' to your playlist.`)
-  }
+    }
 };
--- app/static/view.mjs
+++ app/static/view.mjs
@@ -1,131 +1,131 @@
 const View = {
-  RecommendationButtons: undefined,
-  OnReady: (loadView) => {
-    $(document).ready(loadView());
-  },
-  UpdateCurrentlyPlaying: ({song, artist, album}) => {
-    let elem = $("#track-name");
-    elem.text(`${song.name}`);
+    RecommendationButtons: undefined,
+    OnReady: (loadView) => {
+        $(document).ready(loadView());
+    },
+    UpdateCurrentlyPlaying: ({ song, artist, album }) => {
+        let elem = $("#track-name");
+        elem.text(`${song.name}`);
 
-    elem = $("#artist-name");
-    elem.text(`${artist.name}`);
+        elem = $("#artist-name");
+        elem.text(`${artist.name}`);
 
-    elem = $("#album-name");
-    elem.text(`${album.name}`);
+        elem = $("#album-name");
+        elem.text(`${album.name}`);
 
-    let albumArtElem = $('#album-art');
-    albumArtElem.attr('src', album.coverLink);
+        let albumArtElem = $('#album-art');
+        albumArtElem.attr('src', album.coverLink);
 
-    let songLinkElem = $('#album-art-link');
-    songLinkElem.attr('href', song.link);
+        let songLinkElem = $('#album-art-link');
+        songLinkElem.attr('href', song.link);
 
-    $('#music-metadata-container').css('display', 'block');
-  },
-  PresentSinglePlayButton: ({clickHandler}) => {
-    // default view contains button for playing random song
-    $('#random-song').on('click', () => {
-      setElemClass('random-song', 'loading');
-      clickHandler();
-    });
-  },
-  PresentRecommendationControls: ({recommendationHandler, randomSongHandler}) => {
-    if (View.RecommendationButtons === undefined) {
-      View.RecommendationButtons = getRecommendationButtons(recommendationHandler, randomSongHandler);
+        $('#music-metadata-container').css('display', 'block');
+    },
+    PresentSinglePlayButton: ({ clickHandler }) => {
+        // default view contains button for playing random song
+        $('#random-song').on('click', () => {
+            setElemClass('random-song', 'loading');
+            clickHandler();
+        });
+    },
+    PresentRecommendationControls: ({ recommendationHandler, randomSongHandler }) => {
+        if (View.RecommendationButtons === undefined) {
+            View.RecommendationButtons = getRecommendationButtons(recommendationHandler, randomSongHandler);
+        }
+
+        let controlsContainer = $('#player-controls-container');
+        if (controlsContainer.length === 0) {
+            console.log("ERROR: Could not find location to present recommendation controls.")
+            return;
+        }
+
+        let currentPlayerControls = controlsContainer.children();
+        if (currentPlayerControls.length === 0) {
+            controlsContainer.appendTo(View.RecommendationButtons);
+
+        } else {
+            currentPlayerControls.hide('slow', () => {
+                controlsContainer.append(View.RecommendationButtons);
+                currentPlayerControls.remove();
+            });
+        }
+    },
+
+    PresentPlaylistEditorControls: ({ addSongHandler }) => {
+        let playlistControls = $("#playlist-controls");
+        if (playlistControls.length === 0) {
+            console.log("ERROR: Could not find where to present playlist controls.");
+            return;
+        }
+
+        if (playlistControls.children().length !== 0) {
+            console.log("Aborting presentation of palylist conotrols, as they are already present.");
+        }
+
+        // TODO: https://stackoverflow.com/a/1520352
+        playlistControls.append(
+            $("<button></button>")
+                .attr('id', 'add-song-to-playlist')
+                .addClass('btn btn-primary btn-lg')
+                .text('Save to Playlist')
+                .on('click', () => {
+                    setElemClass('add-song-to-playlist', 'loading');
+                    addSongHandler();
+                })
+        );
+    },
+
+    // code smell: is it really necessary to expose this method? couldn't we instead
+    //             update the state when the View updates?
+    SetState: ({ loading }) => {
+        if (loading === false) {
+            $('.loading').removeClass('loading');
+        }
+    },
+
+    // TODO: present messages in a user-friendly manner
+    PresentMessage: (msg) => {
+        alert(msg);
     }
-
-    let controlsContainer = $('#player-controls-container');
-    if (controlsContainer.length === 0) {
-      console.log("ERROR: Could not find location to present recommendation controls.")
-      return;
-    }
-
-    let currentPlayerControls = controlsContainer.children();
-    if (currentPlayerControls.length === 0) {
-      controlsContainer.appendTo(View.RecommendationButtons);
-
-    } else {
-      currentPlayerControls.hide('slow', () => {
-        controlsContainer.append(View.RecommendationButtons);
-        currentPlayerControls.remove();
-      });
-    }
-  },
-
-  PresentPlaylistEditorControls : ({addSongHandler}) => {
-    let playlistControls = $("#playlist-controls");
-    if (playlistControls.length === 0) {
-      console.log("ERROR: Could not find where to present playlist controls.");
-      return;
-    }
-
-    if (playlistControls.children().length !== 0) {
-      console.log("Aborting presentation of palylist conotrols, as they are already present.");
-    }
-
-    // TODO: https://stackoverflow.com/a/1520352
-    playlistControls.append(
-      $("<button></button>")
-        .attr('id', 'add-song-to-playlist')
-        .addClass('btn btn-primary btn-lg')
-        .text('Save to Playlist')
-        .on('click', () => {
-          setElemClass('add-song-to-playlist', 'loading');
-          addSongHandler();
-        })
-    );
-  },
-
-  // code smell: is it really necessary to expose this method? couldn't we instead
-  //             update the state when the View updates?
-  SetState: ({loading}) => {
-    if (loading === false) {
-      $('.loading').removeClass('loading');
-    }
-  },
-
-  // TODO: present messages in a user-friendly manner
-  PresentMessage: (msg) => {
-    alert(msg);
-  }
 }
 
 const setElemClass = (id, cssClass) => {
-  $('#'+id).addClass(cssClass);
+    $('#' + id).addClass(cssClass);
 }
 
 const getRecommendationButtons = (recommendationHandler, randomSongHandler) => {
-  let recommendationButtons = [];
-  for (let btn of [
-    {adj: undefined,        title: 'More like this',        id: 'similar-rec-btn'},
-    {adj: 'less acoustic',  title: 'More electric',         id: 'more-electric-rec-btn'},
-    {adj: 'more acoustic',  title: 'More acoustic',         id: 'more-acoustic-rec-btn'},
-    {adj: 'less popular',   title: 'More obscure',          id: 'more-obscure-rec-btn'},
-    {adj: 'more popular',   title: 'More mainstream',       id: 'more-popular-rec-btn'},
-    {adj: 'more happy',     title: 'Happier',               id: 'happier-rec-btn'},
-    {adj: 'less happy',     title: 'Sadder',                id: 'sadder-rec-btn'},
-    {adj: 'less dancey',    title: 'Less dancey',           id: 'less-dancey-rec-btn'},
-    {adj: 'more dancey',    title: 'Dancier',               id: 'dancier-rec-btn'},
-  ]) {
+    let recommendationButtons = [];
+    for (let btn of [
+        { adj: undefined, title: 'More like this', id: 'similar-rec-btn' },
+        { adj: 'less acoustic', title: 'More electric', id: 'more-electric-rec-btn' },
+        { adj: 'more acoustic', title: 'More acoustic', id: 'more-acoustic-rec-btn' },
+        { adj: 'less popular', title: 'More obscure', id: 'more-obscure-rec-btn' },
+        { adj: 'more popular', title: 'More mainstream', id: 'more-popular-rec-btn' },
+        { adj: 'more happy', title: 'Happier', id: 'happier-rec-btn' },
+        { adj: 'less happy', title: 'Sadder', id: 'sadder-rec-btn' },
+        { adj: 'less dancey', title: 'Less dancey', id: 'less-dancey-rec-btn' },
+        { adj: 'more dancey', title: 'Dancier', id: 'dancier-rec-btn' },
+    ]) {
+        recommendationButtons.push($('<button></button>')
+            .attr('id', btn.id)
+            .addClass("btn btn-primary btn-sm m-2")
+            .text(btn.title)
+            .on('click', () => {
+                setElemClass(btn.id, 'loading');
+                recommendationHandler({ recommendType: btn.adj });
+            })
+        );
+    }
     recommendationButtons.push($('<button></button>')
-      .attr('id', btn.id)
-      .addClass("btn btn-primary btn-sm m-2")
-      .text(btn.title)
-      .on('click', () => {
-        setElemClass(btn.id, 'loading');
-        recommendationHandler({recommendType: btn.adj});
-      })
+        .attr('id', 'random-rec-btn')
+        .addClass("btn btn-sm m-2")
+        .text("Just random!")
+        .on('click', () => {
+            setElemClass('random-rec-btn', 'loading');
+            randomSongHandler();
+        })
     );
-  }
-  recommendationButtons.push($('<button></button>')
-    .attr('id', 'random-rec-btn')
-    .addClass("btn btn-sm m-2")
-    .text("Just random!")
-    .on('click', () => {
-      setElemClass('random-rec-btn', 'loading');
-      randomSongHandler();
-    })
-  );
-  return recommendationButtons;
+    return recommendationButtons;
 };
 
 export { View }
